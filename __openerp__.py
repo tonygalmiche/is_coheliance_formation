cbc479531e992e53f78ecd6c54360f14b2026019
Tony Galmiche <tony.galmiche@infosaone.com>
2017-03-08 09:03:43 +0100

    mise en ligne

---
+++ __openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+  "name" : "InfoSaône - Module Odoo pour Coheliance Formation",
+  "version" : "0.1",
+  "author" : "InfoSaône / Tony Galmiche",
+  "category" : "InfoSaône",
+  'description': """
+InfoSaône - Module Odoo pour Coheliance Formation
+===================================================
+
+InfoSaône - Module Odoo pour Coheliance Formation
+""",
+  'maintainer': 'InfoSaône',
+  'website': 'http://www.infosaone.com',
+
+  "depends" : [
+    "is_coheliance",
+  ], 
+  "init_xml" : [],            
+  "demo_xml" : [],            
+  "data" : [
+    "views/layouts.xml",
+  ],                           
+  "installable": True,         
+  "active": False,            
+  "application": True,
+}
+
